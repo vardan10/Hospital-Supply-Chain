5d3b87f4916e6380178bec211a5e3e14c023e388
Vardan Nadkarni <vardan_nadkarni@persistent.co.in>
2018-09-06 14:03:58 +0530

    Login functionality

--- app.js
+++ app.js
@@ -109,9 +109,11 @@ function getErrorMessage(field) {
 app.post('/users', async function(req, res) {
 	var username = req.body.username;
 	var orgName = req.body.orgName;
+	var password = req.body.secret;
 	logger.debug('End point : /users');
 	logger.debug('User name : ' + username);
 	logger.debug('Org name  : ' + orgName);
+	logger.debug('password  : ' + password);
 	if (!username) {
 		res.json(getErrorMessage('\'username\''));
 		return;
@@ -125,7 +127,7 @@ app.post('/users', async function(req, res) {
 		username: username,
 		orgName: orgName
 	}, app.get('secret'));
-	let response = await helper.getRegisteredUser(username, orgName, true);
+	let response = await helper.getRegisteredUser(username, orgName, password, true);
 	logger.debug('-- returned from registering the username %s for organization %s',username,orgName);
 	if (response && typeof response !== 'string') {
 		logger.debug('Successfully registered the username %s for organization %s',username,orgName);
