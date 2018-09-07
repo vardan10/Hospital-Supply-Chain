810d038d27a63afed5e9d11863c3084686d9992d
root <root@in-ibmibm3327.persistent.co.in>
2018-09-07 14:15:59 +0530

    Code cleanup

--- artifacts/src/github.com/example_cc/go/example_cc.go
+++ artifacts/src/github.com/example_cc/go/example_cc.go
@@ -1,108 +1,9 @@
-/*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-*/
-
-// ====CHAINCODE EXECUTION SAMPLES (CLI) ==================
-
-// ==== Invoke marbles ====
-// peer chaincode invoke -C myc1 -n marbles -c '{"Args":["initMarble","marble1","blue","35","tom"]}'
-// peer chaincode invoke -C myc1 -n marbles -c '{"Args":["initMarble","marble2","red","50","tom"]}'
-// peer chaincode invoke -C myc1 -n marbles -c '{"Args":["initMarble","marble3","blue","70","tom"]}'
-// peer chaincode invoke -C myc1 -n marbles -c '{"Args":["transferMarble","marble2","jerry"]}'
-// peer chaincode invoke -C myc1 -n marbles -c '{"Args":["transferMarblesBasedOnColor","blue","jerry"]}'
-// peer chaincode invoke -C myc1 -n marbles -c '{"Args":["delete","marble1"]}'
-
-// ==== Query marbles ====
-// peer chaincode query -C myc1 -n marbles -c '{"Args":["readMarble","marble1"]}'
-// peer chaincode query -C myc1 -n marbles -c '{"Args":["getMarblesByRange","marble1","marble3"]}'
-// peer chaincode query -C myc1 -n marbles -c '{"Args":["getHistoryForMarble","marble1"]}'
-
-// Rich Query (Only supported if CouchDB is used as state database):
-//   peer chaincode query -C myc1 -n marbles -c '{"Args":["queryMarblesByOwner","tom"]}'
-//   peer chaincode query -C myc1 -n marbles -c '{"Args":["queryMarbles","{\"selector\":{\"owner\":\"tom\"}}"]}'
-
-// INDEXES TO SUPPORT COUCHDB RICH QUERIES
-//
-// Indexes in CouchDB are required in order to make JSON queries efficient and are required for
-// any JSON query with a sort. As of Hyperledger Fabric 1.1, indexes may be packaged alongside
-// chaincode in a META-INF/statedb/couchdb/indexes directory. Each index must be defined in its own
-// text file with extension *.json with the index definition formatted in JSON following the
-// CouchDB index JSON syntax as documented at:
-// http://docs.couchdb.org/en/2.1.1/api/database/find.html#db-index
-//
-// This marbles02 example chaincode demonstrates a packaged
-// index which you can find in META-INF/statedb/couchdb/indexes/indexOwner.json.
-// For deployment of chaincode to production environments, it is recommended
-// to define any indexes alongside chaincode so that the chaincode and supporting indexes
-// are deployed automatically as a unit, once the chaincode has been installed on a peer and
-// instantiated on a channel. See Hyperledger Fabric documentation for more details.
-//
-// If you have access to the your peer's CouchDB state database in a development environment,
-// you may want to iteratively test various indexes in support of your chaincode queries.  You
-// can use the CouchDB Fauxton interface or a command line curl utility to create and update
-// indexes. Then once you finalize an index, include the index definition alongside your
-// chaincode in the META-INF/statedb/couchdb/indexes directory, for packaging and deployment
-// to managed environments.
-//
-// In the examples below you can find index definitions that support marbles02
-// chaincode queries, along with the syntax that you can use in development environments
-// to create the indexes in the CouchDB Fauxton interface or a curl command line utility.
-//
-
-//Example hostname:port configurations to access CouchDB.
-//
-//To access CouchDB docker container from within another docker container or from vagrant environments:
-// http://couchdb:5984/
-//
-//Inside couchdb docker container
-// http://127.0.0.1:5984/
-
-// Index for docType, owner.
-// Note that docType and owner fields must be prefixed with the "data" wrapper
-//
-// Index definition for use with Fauxton interface
-// {"index":{"fields":["data.docType","data.owner"]},"ddoc":"indexOwnerDoc", "name":"indexOwner","type":"json"}
-//
-// Example curl command line to define index in the CouchDB channel_chaincode database
-// curl -i -X POST -H "Content-Type: application/json" -d "{\"index\":{\"fields\":[\"data.docType\",\"data.owner\"]},\"name\":\"indexOwner\",\"ddoc\":\"indexOwnerDoc\",\"type\":\"json\"}" http://hostname:port/myc1_marbles/_index
-//
-
-// Index for docType, owner, size (descending order).
-// Note that docType, owner and size fields must be prefixed with the "data" wrapper
-//
-// Index definition for use with Fauxton interface
-// {"index":{"fields":[{"data.size":"desc"},{"data.docType":"desc"},{"data.owner":"desc"}]},"ddoc":"indexSizeSortDoc", "name":"indexSizeSortDesc","type":"json"}
-//
-// Example curl command line to define index in the CouchDB channel_chaincode database
-// curl -i -X POST -H "Content-Type: application/json" -d "{\"index\":{\"fields\":[{\"data.size\":\"desc\"},{\"data.docType\":\"desc\"},{\"data.owner\":\"desc\"}]},\"ddoc\":\"indexSizeSortDoc\", \"name\":\"indexSizeSortDesc\",\"type\":\"json\"}" http://hostname:port/myc1_marbles/_index
-
-// Rich Query with index design doc and index name specified (Only supported if CouchDB is used as state database):
-//   peer chaincode query -C myc1 -n marbles -c '{"Args":["queryMarbles","{\"selector\":{\"docType\":\"marble\",\"owner\":\"tom\"}, \"use_index\":[\"_design/indexOwnerDoc\", \"indexOwner\"]}"]}'
-
-// Rich Query with index design doc specified only (Only supported if CouchDB is used as state database):
-//   peer chaincode query -C myc1 -n marbles -c '{"Args":["queryMarbles","{\"selector\":{\"docType\":{\"$eq\":\"marble\"},\"owner\":{\"$eq\":\"tom\"},\"size\":{\"$gt\":0}},\"fields\":[\"docType\",\"owner\",\"size\"],\"sort\":[{\"size\":\"desc\"}],\"use_index\":\"_design/indexSizeSortDoc\"}"]}'
-
 package main
 
 import (
 	"encoding/json"
 	"fmt"
-	"strings"
+	"bytes"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
@@ -113,7 +14,6 @@ type SimpleChaincode struct {
 }
 
 type marble struct {
-	ObjectType			string `json:"docType"` 		//docType is used to distinguish the various types of objects in state database
 	Name       			string `json:"name"`			//the fieldtags are needed to keep case from bouncing around
 	HospitalName		string `json:"hospitalName"`
 	AssetRequested      string `json:"assetRequested"`
@@ -154,13 +54,13 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 }
 
 // ============================================================
-// initMarble - create a new marble, store into chaincode state
+// hospitalInvoke - Hospital Creates a request
 // ============================================================
 func (t *SimpleChaincode) hospitalInvoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	var err error
 
-	//   0       1       2     3
-	// "asdf", "blue", "bob1", "bob2"
+	//   0       	1       		2     				3
+	// "REQ1", "HospitalName", "AssetRequested", "VolunteerName"
 	if len(args) != 4 {
 		return shim.Error("Incorrect number of arguments. Expecting 4")
 	}
@@ -179,68 +79,70 @@ func (t *SimpleChaincode) hospitalInvoke(stub shim.ChaincodeStubInterface, args 
 	if len(args[3]) <= 0 {
 		return shim.Error("4th argument must be a non-empty string")
 	}
-	marbleName := args[0]
-	color := strings.ToLower(args[1])
-	owner := strings.ToLower(args[2])
-	size  := strings.ToLower(args[3])
 
-	// ==== Create marble object and marshal to JSON ====
-	objectType := "marble"
-	marble := &marble{objectType, marbleName, color, owner, size}
-	marbleJSONasBytes, err := json.Marshal(marble)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	//Alternatively, build the marble json string manually if you don't want to use struct marshalling
-	//marbleJSONasString := `{"docType":"Marble",  "name": "` + marbleName + `", "color": "` + color + `", "size": ` + strconv.Itoa(size) + `, "owner": "` + owner + `"}`
-	//marbleJSONasBytes := []byte(str)
+	Requestkey := args[0]
+	hName := args[1]
+	asset := args[2]
+	vName  := args[3]
 
-	// === Save marble to state ===
-	err = stub.PutState(marbleName, marbleJSONasBytes)
+	request := &marble{Requestkey, hName, asset, vName}
+	requestJSONasBytes, err := json.Marshal(request)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	// //  ==== Index the marble to enable color-based range queries, e.g. return all blue marbles ====
-	// //  An 'index' is a normal key/value entry in state.
-	// //  The key is a composite key, with the elements that you want to range query on listed first.
-	// //  In our case, the composite key is based on indexName~color~name.
-	// //  This will enable very efficient state range queries based on composite keys matching indexName~color~*
-	// indexName := "color~name"
-	// colorNameIndexKey, err := stub.CreateCompositeKey(indexName, []string{marble.Color, marble.Name})
-	// if err != nil {
-	// 	return shim.Error(err.Error())
-	// }
-	// //  Save index entry to state. Only the key name is needed, no need to store a duplicate copy of the marble.
-	// //  Note - passing a 'nil' value will effectively delete the key from state, therefore we pass null character as value
-	// value := []byte{0x00}
-	// stub.PutState(colorNameIndexKey, value)
+	err = stub.PutState(Requestkey, requestJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
 
-	// ==== Marble saved and indexed. Return success ====
-	fmt.Println("- end init marble")
 	return shim.Success(nil)
 }
 
-// ===============================================
-// readMarble - read a marble from chaincode state
-// ===============================================
+// ================================================
+// hospitalQuery - Get all Hospital request records
+// ================================================
 func (t *SimpleChaincode) hospitalQuery(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var name, jsonResp string
-	var err error
 
-	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting name of the marble to query")
+	if len(args) < 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
 
-	name = args[0]
-	valAsbytes, err := stub.GetState(name) //get the marble from chaincode state
+	startKey := args[0]
+	endKey := args[1]
+
+	resultsIterator, err := stub.GetStateByRange(startKey, endKey)
 	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
-		return shim.Error(jsonResp)
-	} else if valAsbytes == nil {
-		jsonResp = "{\"Error\":\"Marble does not exist: " + name + "\"}"
-		return shim.Error(jsonResp)
+		return shim.Error(err.Error())
 	}
+	defer resultsIterator.Close()
 
-	return shim.Success(valAsbytes)
+	// buffer is a JSON array containing QueryResults
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	return shim.Success(buffer.Bytes())
 }
--- testAPIs.sh
+++ testAPIs.sh
@@ -151,7 +151,7 @@ curl -s -X POST \
 	\"chaincodeName\":\"mycc\",
 	\"chaincodePath\":\"$CC_SRC_PATH\",
 	\"chaincodeType\": \"$LANGUAGE\",
-	\"chaincodeVersion\":\"v7\"
+	\"chaincodeVersion\":\"v8\"
 }"
 echo
 echo
@@ -167,7 +167,7 @@ curl -s -X POST \
 	\"chaincodeName\":\"mycc\",
 	\"chaincodePath\":\"$CC_SRC_PATH\",
 	\"chaincodeType\": \"$LANGUAGE\",
-	\"chaincodeVersion\":\"v7\"
+	\"chaincodeVersion\":\"v8\"
 }"
 echo
 echo
@@ -183,7 +183,7 @@ echo
 # 	\"chaincodeName\":\"mycc\",
 # 	\"chaincodePath\":\"$CC_SRC_PATH\",
 # 	\"chaincodeType\": \"$LANGUAGE\",
-# 	\"chaincodeVersion\":\"v7\"
+# 	\"chaincodeVersion\":\"v8\"
 # }"
 # echo
 # echo
@@ -196,7 +196,7 @@ curl -s -X POST \
   -H "content-type: application/json" \
   -d "{
 	\"chaincodeName\":\"mycc\",
-	\"chaincodeVersion\":\"v7\",
+	\"chaincodeVersion\":\"v8\",
 	\"chaincodeType\": \"$LANGUAGE\",
 	\"args\":[\"a\",\"100\",\"b\",\"200\"]
 }"
